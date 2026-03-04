8ac239eb24d81c4d2f665e9cf9c75625c7fe6125
Nathan Moran <20107318@setu.ie>
2026-03-04 21:16:37 +0000

    Refactor: Template list page; HTTP API calls centralized.

--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -3,6 +3,7 @@ import MovieList from "../components/movieList";
 import Grid from "@mui/material/Grid";
 import Header from '../components/headerMovieList';
 import FilterCard from "../components/filterMoviesCard";
+import { getMovies } from "../api/tmdb-api";
 
 const HomePage = (props) => {
   const [movies, setMovies] = useState([]);
@@ -33,17 +34,9 @@ const HomePage = (props) => {
   };
 
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(json);
-        return json.results;
-      })
-      .then((movies) => {
-        setMovies(movies);
-      });
+    getMovies().then(movies => {
+      setMovies(movies);
+    });
   }, []);
 
   return (
