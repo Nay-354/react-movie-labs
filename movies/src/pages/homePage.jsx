ec98e664ee3174e9493778e37b76de310e917253
Nathan Moran <20107318@setu.ie>
2026-03-11 15:57:32 +0000

    Using react-query to manage the browser-based cache.

--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -1,60 +1,37 @@
-import React, { useState, useEffect } from "react";  
-import MovieList from "../components/movieList";
-import Grid from "@mui/material/Grid";
-import Header from '../components/headerMovieList';
-import FilterCard from "../components/filterMoviesCard";
+import React from "react";
 import { getMovies } from "../api/tmdb-api";
+import PageTemplate from '../components/templateMovieListPage';
+import { useQuery } from '@tanstack/react-query';
+import Spinner from '../components/spinner';
 
 const HomePage = (props) => {
-  const [movies, setMovies] = useState([]);
 
-    const [nameFilter, setNameFilter] = useState("");
-  const [genreFilter, setGenreFilter] = useState("0");
+  const { data, error, isPending, isError  } = useQuery({
+    queryKey: ['discover'],
+    queryFn: getMovies,
+  })
+  
+  if (isPending) {
+    return <Spinner />
+  }
 
-    const genreId = Number(genreFilter);
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  
+  const movies = data.results;
 
-  let displayedMovies = movies
-    .filter((m) => {
-      return m.title.toLowerCase().search(nameFilter.toLowerCase()) !== -1;
-    })
-    .filter((m) => {
-      return genreId > 0 ? m.genre_ids.includes(genreId) : true;
-    });
-
-  const handleChange = (type, value) => {
-    if (type === "name") setNameFilter(value);
-    else setGenreFilter(value);
-  };
-
-  const addToFavorites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favorite: true } : m
-    );
-    setMovies(updatedMovies);
-  };
-
-  useEffect(() => {
-    getMovies().then(movies => {
-      setMovies(movies);
-    });
-  }, []);
+  // Redundant, but necessary to avoid app crashing.
+  const favorites = movies.filter(m => m.favorite)
+  localStorage.setItem('favorites', JSON.stringify(favorites))
+  const addToFavorites = (movieId) => true 
 
   return (
-    <Grid container>
-      <Grid size={12}>
-        <Header title={"Home Page"} />
-      </Grid>
-      <Grid container sx={{flex: "1 1 500px"}}>
-        <Grid key="find" size={{xs: 12, sm: 6, md: 4, lg: 3, xl: 2}} sx={{padding: "20px"}}>
-             <FilterCard
-      onUserInput={handleChange}
-      titleFilter={nameFilter}
-      genreFilter={genreFilter}
+    <PageTemplate
+      title='Discover Movies'
+      movies={movies}
+      selectFavorite={addToFavorites}
     />
-        </Grid>
-    <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
-      </Grid>
-    </Grid>
   );
 };
 export default HomePage;
