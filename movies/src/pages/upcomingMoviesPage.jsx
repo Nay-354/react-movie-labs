a28fe49b26efc90c9dbb0152cf67092f175c032c
Nathan Moran <20107318@setu.ie>
2026-03-18 16:02:00 +0000

    New button for upcoming movies page.

--- movies/src/pages/upcomingMoviesPage.jsx
+++ movies/src/pages/upcomingMoviesPage.jsx
@@ -4,6 +4,7 @@ import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from '@tanstack/react-query';
 import Spinner from '../components/spinner';
 import AddToFavoritesIcon from '../components/cardIcons/addToFavorites';
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd'
 
 const UpcomingPage = (props) => {
 
@@ -23,16 +24,16 @@ const UpcomingPage = (props) => {
   const upcomingMovies = data.results;
 
   // Redundant, but necessary to avoid app crashing.
-  const favorites = upcomingMovies.filter(uM => uM.favorite)
-  localStorage.setItem('favorites', JSON.stringify(favorites))
-  const addToFavorites = (upcomingMovieId) => true 
+  const playlists = upcomingMovies.filter(uM => uM.playlist)
+  localStorage.setItem('playlists', JSON.stringify(playlists))
+  const PlaylistAdd = (upcomingMovieId) => true 
 
    return (
       <PageTemplate
         title="Upcoming Movies"
         movies={upcomingMovies}
         action={(upcomingMovie) => {
-          return <AddToFavoritesIcon upcomingMovie={upcomingMovie} />
+          return <PlaylistAddIcon upcomingMovie={upcomingMovie} />
         }}
       />
   );
